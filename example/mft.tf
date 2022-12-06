4f2e339af96fa9ab1cd2967146ca4f88d7b527b3
Ryan <lin827895450@gmail.com>
2022-12-07 00:33:53 +1100

    test

--- example/mft.tf
+++ example/mft.tf
@@ -2,8 +2,8 @@ module "sftp" {
   source = "git::https://github.com/ryanlin-tech/mft-tf-module.git"
 
   account_name         = "mft-test"
-  domain_host          = "staging.easyreturns.com.au"
-  domain_zone          = "Z016568416TDE7NM23XU5"
+  domain_host          = "mft.staging.easyreturns.com.au"
+  domain_zone          = "staging.easyreturns.com.au"
   s3_bucket_name       = "mft-s3-test"
   s3_bucket_versioning = true
   security_policy_name = "TransferSecurityPolicy-2018-11"
