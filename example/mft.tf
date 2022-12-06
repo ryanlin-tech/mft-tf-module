2ee4f876611c1bd7959ced4b7be8ae57ebe72951
Ryan <lin827895450@gmail.com>
2022-12-07 00:13:57 +1100

    test

--- example/mft.tf
+++ example/mft.tf
@@ -1,5 +1,5 @@
 module "sftp" {
-  source = "git::git@github.com:ryanlin-tech/mft-tf-module.git"
+  source = "git::https://github.com/ryanlin-tech/mft-tf-module.git"
 
   account_name         = "mft-test"
   domain_host          = "staging.easyreturns.com.au"
